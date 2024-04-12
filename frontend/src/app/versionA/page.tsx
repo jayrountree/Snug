34cbe4e9d99c13102af789bb9c1d7d362753e679
Jay Rountree <jaynr22@gmail.com>
2024-04-12 16:49:55 -0500

    basic search

--- frontend/src/app/versionA/page.tsx
+++ frontend/src/app/versionA/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, query, where } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 
 export interface PostInterface {
@@ -17,22 +17,43 @@ const Home = () => {
   const db = getFirestore();
   const [data, setData] = useState<PostInterface[]>([]);
 
+  const [searchWords, setSearchWords] = useState([""]);
+  const [search, setSearch] = useState("");
+  
   useEffect(() => {
     const fetchData = async () => {
+      // Fetch all posts initially
       const querySnapshot = await getDocs(collection(db, "all-posts"));
-      const newData = querySnapshot.docs.map(
-        (doc) => doc.data() as PostInterface
-      );
-      setData(newData);
+      const allPosts = querySnapshot.docs.map((doc) => doc.data() as PostInterface);
+
+      if (!search.trim()) {
+        setData(allPosts);
+      } else {
+        // Filter posts based on tags
+        const q = query(collection(db, "all-posts"), where('itemTags', 'array-contains-any', search.trim().split(' ')));
+        const filteredQuerySnapshot = await getDocs(q);
+        const filteredPosts = filteredQuerySnapshot.docs.map((doc) => doc.data() as PostInterface);
+        setData(filteredPosts);
+      }
     };
 
     fetchData();
-  }, []);
+  }, [db, searchWords]);
 
   console.log(data);
 
   return (
     <div className="flex flex-col gap-2 justify-center items-center">
+      <div>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button onClick={() => {
+          setSearchWords(search.split(' '));
+        }}>Search</button>
+      </div>
+      
       {data.map((i, index) => (
         <img
           key={index}
